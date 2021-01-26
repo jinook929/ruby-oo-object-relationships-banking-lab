77591a1d65371c8415b10ec10babeae3f64a67e2
jinook929 <jinook929@gmail.com>
2021-01-25 19:56:51 -0500

    commit tree f1b0370d1541fa2e27881a0e0609a4a58ef17b50

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+    @@all = []
+    def self.all
+        @@all
+    end
 
+    attr_accessor :balance, :status
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        self.class.all.push(self)
+    end
+
+    def deposit(amount)
+        self.balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+        self.class.all.delete_if {|account| account == self}
+    end
 end
